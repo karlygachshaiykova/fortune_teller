090692a582bfc4b7f0c7b3906c7930176baa7886
Karlygach Shaiykova <kshaiykova@gmail.com>
2020-08-13 01:53:44 -0700

    fortune_teller_project

---
+++ index.js
@@ -0,0 +1,5 @@
+var children = 5;
+var partner = 'Kaku';
+var city = 'Redmond';
+var job ='Software Engineer';
+console.log('You will be a ' + job + ' in ' + city + ' and married to ' + partner +' with '+ children);
